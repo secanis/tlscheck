09a483be0312c39a01b882ae6777034ab6001bdf
Matthias Baldi <matthias.baldi@secanis.ch>
2026-03-04 00:49:05 +0100

    docs: improve docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -32,6 +32,8 @@ export default defineConfig({
           label: 'Getting Started',
           items: [
             { label: 'Introduction', link: '/getting-started/' },
+            { label: 'Privacy Policy', link: '/privacy/' },
+            { label: 'License', link: '/license/' },
           ],
         },
         {
@@ -46,8 +48,6 @@ export default defineConfig({
             { label: 'UI Colors', link: '/ui-colors/' },
             { label: 'Container', link: '/docker/' },
             { label: 'CI/CD', link: '/tests/' },
-            { label: 'License', link: '/license/' },
-            { label: 'Privacy Policy', link: '/privacy/' },
             ...openAPISidebarGroups,
           ],
         },
